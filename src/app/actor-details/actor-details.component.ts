87e1e1a59fce242aed28db2a3af18d8a7c4ba7c1
Mikołaj Zuziak <m.w.zuziak@gmail.com>
2023-07-05 13:48:44 +0200

    feat(Frontend): :zap: add content to actor details view
    add content to actor detail view base on actor id passed in url

--- src/app/actor-details/actor-details.component.ts
+++ src/app/actor-details/actor-details.component.ts
@@ -1,10 +1,38 @@
-import { Component } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { Location } from '@angular/common';
+import { ActivatedRoute } from '@angular/router';
+import { Actor } from 'src/interfaces/actor.interface';
+import { ActorService } from '../services/actor.service';
 
 @Component({
   selector: 'app-actor-details',
   templateUrl: './actor-details.component.html',
-  styleUrls: ['./actor-details.component.css']
+  styleUrls: ['./actor-details.component.css'],
 })
-export class ActorDetailsComponent {
+export class ActorDetailsComponent implements OnInit {
+  @Input() actor?: Actor;
 
+  constructor(
+    private actorService: ActorService,
+    private location: Location,
+    private route: ActivatedRoute
+  ) {}
+
+  ngOnInit(): void {
+    const id = Number(this.route.snapshot.paramMap.get('id'));
+
+    this.actorService
+      .getActorById(id)
+      .subscribe((actor) => (this.actor = actor));
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
+
+  save(): void {
+    if (!this.actor) return;
+
+    this.actorService.updateActor(this.actor).subscribe(() => this.goBack());
+  }
 }
